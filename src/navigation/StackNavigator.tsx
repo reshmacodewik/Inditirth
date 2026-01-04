c40be0f5e77f7579f08ca3f43d1c60d6f1d1aa4f
Sanjay Maurya <sanjaymaurya201@gmail.com>
2026-01-04 15:41:44 +0530

    working

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,4 +1,3 @@
-// src/navigation/StackNavigator.tsx
 import React, { useState, useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from '../screens/SplashScreen';
@@ -20,22 +19,25 @@ export type RootStackParamList = {
   BoatBookingScreen: undefined;
   BookingSummary: undefined;
   Confirmed: undefined;
-  Profile:undefined;
+  Profile: undefined;
 };
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const StackNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      initialRouteName="Splash"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Otp" component={OtpScreen} />
       <Stack.Screen name="Signup" component={SignupScreen} />
-
       <Stack.Screen name="Home" component={BottomTabNavigator} />
       <Stack.Screen name="BoatBookingScreen" component={BoatBookingScreen} />
       <Stack.Screen name="BookingSummary" component={BookingSummaryScreen} />
       <Stack.Screen name="Confirmed" component={BookingConfirmedScreen} />
-      <Stack.Screen name="Profile" component={MyProfileScreen}/>
+      <Stack.Screen name="Profile" component={MyProfileScreen} />
     </Stack.Navigator>
   );
 };
